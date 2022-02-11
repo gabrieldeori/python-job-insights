ae2250796ee466899f8dbd7b07e5353eb593cc94
gabrieldeori <deori.pessoal@gmail.com>
2022-02-11 15:41:52 -0300

    feat: A página está renderizando corretamente, mas não passa no teste

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -11,7 +11,7 @@ from .insights import (
     get_min_salary,
     get_max_salary,
 )
-from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls
+from .more_insights import slice_jobs, get_int_from_args, build_jobs_urls, get_job
 
 bp = Blueprint("client", __name__, template_folder="templates")
 
@@ -58,6 +58,11 @@ def list_jobs():
 
     return render_template("list_jobs.jinja2", ctx=ctx)
 
+@bp.route("/job/<index>")
+def job_details(index):
+    jobs = read(path="src/jobs.csv")
+    job = get_job(jobs, index)
+    return render_template("job.jinja2", job=job)
 
 def init_app(app: Flask):
     app.register_blueprint(bp)
