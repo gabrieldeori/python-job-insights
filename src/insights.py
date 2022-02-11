c81a6a27b363e6551dc41f59be8b39e77f16cacd
gabrieldeori <deori.pessoal@gmail.com>
2022-02-11 00:40:36 -0300

    feat: Chamando função para procurar tipo único de indústrias

--- src/insights.py
+++ src/insights.py
@@ -40,21 +40,8 @@ def filter_by_job_type(jobs, job_type):
 
 
 def get_unique_industries(path):
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    return []
+    industries_type_list = get_uniques(path, 'industry')
+    return industries_type_list
 
 
 def filter_by_industry(jobs, industry):
