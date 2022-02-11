8997cb65713b0869534acc57bfbb8f8e5b0ce391
gabrieldeori <deori.pessoal@gmail.com>
2022-02-11 16:00:18 -0300

    fix: Renomeando função de job_details para job como pede no teste 11

--- src/routes_and_views.py
+++ src/routes_and_views.py
@@ -58,11 +58,13 @@ def list_jobs():
 
     return render_template("list_jobs.jinja2", ctx=ctx)
 
+
 @bp.route("/job/<index>")
-def job_details(index):
+def job(index):
     jobs = read(path="src/jobs.csv")
     job = get_job(jobs, index)
     return render_template("job.jinja2", job=job)
+
 
 def init_app(app: Flask):
     app.register_blueprint(bp)
