de588c03221389358962e27db9456b3a6521e775
gabrieldeori <deori.pessoal@gmail.com>
2022-02-11 00:20:47 -0300

    feat: Lendo arquivo no caminho especificado e retornando lista de empregos

--- src/jobs.py
+++ src/jobs.py
@@ -1,18 +1,13 @@
 from functools import lru_cache
-
+import csv
 
 @lru_cache
 def read(path):
-    """Reads a file from a given path and returns its contents
-
-    Parameters
-    ----------
-    path : str
-        Full path to file
-
-    Returns
-    -------
-    list
-        List of rows as dicts
-    """
-    return []
+    try:
+        with open(path) as archive:
+            jobs_csv = csv.DictReader(archive, delimiter=",", quotechar='"')
+            jobs_list = [job for job in jobs_csv]
+            return jobs_list
+    except OSError:
+        print('Algo deu errado')
+        return False
